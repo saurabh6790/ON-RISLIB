09f155c5f37b5100663c2048cbac8b96d1477966
Saurabh <saurabh.p@indictranstech.com>
2014-12-01 15:59:25 +0530

    fixes

--- core/doctype/db_sync/db_sync.py
+++ core/doctype/db_sync/db_sync.py
@@ -8,6 +8,7 @@ import webnotes
 from install_erpnext import exec_in_shell
 from webnotes.utils import get_base_path, today, cint
 import os
+import MySQLdb
 # import pxssh
 
 from webnotes.utils import cint, cstr, getdate, now, nowdate, get_defaults
@@ -19,7 +20,8 @@ class DocType:
 
 	def sync_db(self, patient_id=None):
 		if not (os.path.exists(os.path.join(get_base_path(), "public", 'dbsync.txt'))):
-			if self.validate_tenant():pass
+			self.validate_tenant()
+			self.initiate_sync_db(patient_id)
 		else:
 			self.initiate_sync_db(patient_id)
 
@@ -31,21 +33,25 @@ class DocType:
 			
 			cursor.execute("select no_of_offline_tenant from `tabSite Details` where name = '%s' "%(self.doc.host_id))		
 			no_of_offline_tenant = cursor.fetchone()
-			
+			webnotes.errprint(no_of_offline_tenant[0][0])
 			cursor.execute("select ifnull(offline_tenant_id, '') from `tabSite Details` where name = '%s' "%(self.doc.host_id))		
 			offline_tenant_id = cursor.fetchone()
 
-			if cint(no_of_offline_tenant) > 0:
+			if cint(no_of_offline_tenant[0][0]) > 0:
 				if offline_tenant_id:
-					tenant_list = offline_tenant_id.split(',')
+					webnotes.errprint(offline_tenant_id[0])
+					tenant_list = offline_tenant_id[0].split(',')
 
-				if len(tenant_list) > cint(no_of_offline_tenant):
+				if len(tenant_list) > cint(no_of_offline_tenant[0][0]):
 					webnotes.msgprint("Tenat Limit Exceeded!!!", raise_exception=1)
 				else:
-					branch_id = webnotes.conn.get_value('Global Details', None, 'branch_id')
+					branch_id = webnotes.conn.get_value('Global Defaults', None, 'branch_id')
 					tenant_list.append(branch_id)
-					cursor.execute("Update `tabSite Details` set offline_tenant_id = '%s' where name = '%s'"%(','.join(tenant_list), self.doc.host_id))
+					webnotes.errprint([tenant_list, branch_id])
+					cursor.execute("Update `tabSite Details` set offline_tenant_id = '%s' where name = '%s'"%(','.join(x for x in tenant_list if x), self.doc.host_id))
 					cursor.execute("commit")
+			else:
+				webnotes.msgprint("Offline tenant not register with this system", raise_exception=1)
 
 	def initiate_sync_db(self, patient_id=None):
 		cond = ''
@@ -70,7 +76,6 @@ class DocType:
 				# webnotes.errprint("""mysql -u %(dbuser)s -p'%(dbuserpassword)s' %(dbname)s < %(file_path)s/up%(file_name)s.sql"""%local_settings)
 				exec_in_shell("""mysqldump --host='%(host_id)s'  -u %(remote_dbuser)s -p'%(remote_dbuserpassword)s' %(remote_dbname)s -t --replace "%(tab)s" %(cond)s > %(file_path)s/up%(file_name)s.sql"""%remote_settings)
 				exec_in_shell("""mysql -u %(dbuser)s -p'%(dbuserpassword)s' %(dbname)s < %(file_path)s/up%(file_name)s.sql"""%local_settings)
-				self.set_sync_date()
 			except Exception as inst: 
 				webnotes.msgprint(inst)
 
@@ -78,7 +83,6 @@ class DocType:
 			self.sync_active_status()
 
 	def sync_active_status(self):
-		import MySQLdb
 		is_active = ''
 		webnotes.errprint([self.doc.host_id, self.doc.remote_dbuser, self.doc.remote_dbuserpassword, self.doc.remote_dbname])
 		db = MySQLdb.connect(self.doc.host_id, self.doc.remote_dbuser, self.doc.remote_dbuserpassword, self.doc.remote_dbname)
@@ -98,6 +102,7 @@ class DocType:
 				# webnotes.errprint("""mysqldump -u %(dbuser)s -p'%(dbuserpassword)s' %(dbname)s -t --replace "%(tab)s" > %(file_path)s/dw%(file_name)s.sql"""%local_settings)
 				exec_in_shell("""mysqldump -u %(dbuser)s -p'%(dbuserpassword)s' %(dbname)s -t --replace "%(tab)s" > %(file_path)s/dw%(file_name)s.sql"""%local_settings)
 				exec_in_shell("""mysql --host='%(host_id)s'  -u %(remote_dbuser)s -p'%(remote_dbuserpassword)s' %(remote_dbname)s < %(file_path)s/dw%(file_name)s.sql"""%remote_settings)
+				self.set_sync_date()
 			except Exception as inst: pass
 
 	def get_remote_settings(self, table, cond=None, patient_id=None):
